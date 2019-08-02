2f7258ead7425d5516700922e7ed6feb085c2913
Nicole Parrish <nicoleparrish.parrish@gmail.com>
2019-08-02 17:03:22 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,10 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+class Dance
+  include Dance 
+  attr_accessor :name 
+  def initialize(name)
+    @name = name 
+    extend MetaDancing 
+  end 
+end 
